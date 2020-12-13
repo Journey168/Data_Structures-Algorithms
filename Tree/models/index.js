bf844f1cca5c15856ee8cf07f8cbd98f22834653
Journey <1274049755@qq.com>
2020-12-13 16:19:06 +0800

    add: 红黑树

--- Tree/models/index.js
+++ Tree/models/index.js
@@ -1,9 +1,25 @@
 
+export const Colors = {
+  RED: 'red',
+  BLACK: 'black'
+}
 
 export class Node {
   constructor(key) {
     this.key = key;
     this.left = null;
     this.right = null;
+  }
+}
+
+export class RedBlackNode extends Node {
+  constructor(key) {
+    super(key);
+    this.key = key;
+    this.color = Colors.RED;
+    this.parent = null
+  }
+  isRed() {
+    return this.color === Colors.RED
   }
 }
